98374d53da93ce40dec56d9a0cf684288e40f225
lovaraj02 <rajubandaru2255@gmail.com>
2025-08-13 10:28:37 +0530

    initial commit

---
+++ functions/functions.py
@@ -0,0 +1,15 @@
+def fun(a):
+    print("This is number",a)
+fun(1)
+fun(2)
+
+def fun(a,b):
+    print("Addition of two nums :",a+b)
+fun(2,3)
+
+def fun(name = "raju"):
+    print("My name is :",name)
+fun("Balu")
+fun("Sai")
+fun()
+
---
+++ oops/class.py
@@ -0,0 +1,9 @@
+class raj:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print("my name is :",self.name)
+        print("my age is :",self.age)
+obj1 = raj("Lova Raju",21)
+obj1.info()
