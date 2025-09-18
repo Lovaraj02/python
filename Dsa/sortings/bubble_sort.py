3d3a57784c02f7a178b6dff7bf4abffb301843c8
lovaraj02 <rajubandaru2255@gmail.com>
2025-09-18 08:57:32 +0530

    sortingd

--- Dsa/sortings/bubble_sort.py
+++ Dsa/sortings/bubble_sort.py
@@ -4,3 +4,11 @@ for i in range(len(arr)-1):
         if arr[j]>arr[j+1]:
             arr[j],arr[j+1] = arr[j+1],arr[j]
 print(arr)
+
+
+# arr = [64, 34, 25, 12, 22, 11, 90, 5]
+# for i in range(len(arr)):
+#     for j in range(len(arr)-1):
+#         if arr[j]>arr[j+1]:
+#             arr[j],arr[j+1] = arr[j+1],arr[j]
+# print(arr)
--- oops/inheritance.py
+++ oops/inheritance.py
@@ -17,34 +17,85 @@ obj.details()
 
 
 
+# multilevel
+class GrandMother:
+    def property(self):
+        print("Property from GrandMother")
+class Mother(GrandMother):
+    def skills(self):
+        print("Cooking skills from Mother")
+class Son(Mother):   # inherits both Mother & GrandMother
+    def education(self):
+        print("Education from Son")
+obj = Son()
+obj.property()
+obj.skills()
+obj.education()
 
 
 
 
+#multiple
+class Father:
+    def driving(self):
+        print("Driving skill from Father")
+
+class Mother:
+    def cooking(self):
+        print("Cooking skill from Mother")
+
+class Son(Father, Mother):   # Multiple inheritance
+    def sports(self):
+        print("Sports skill from Son")
+
+obj = Son()
+obj.driving()
+obj.cooking()
+obj.sports()
+ 
+
+# hierarchical
+class Parent:
+    def property(self):
+        print("House from Parent")
+
+class Son(Parent):
+    def job(self):
+        print("Job skill from Son")
+
+class Daughter(Parent):
+    def singing(self):
+        print("Singing talent from Daughter")
+
+s = Son()
+d = Daughter()
+s.property()
+s.job()
+d.property()
+d.singing()
 
 
 
+# hybrid
+class GrandParent:
+    def wisdom(self):
+        print("Wisdom from GrandParent")
 
+class Father(GrandParent):
+    def driving(self):
+        print("Driving skill from Father")
 
+class Mother(GrandParent):
+    def cooking(self):
+        print("Cooking skill from Mother")
 
+class Son(Father, Mother):   # Hybrid inheritance
+    def sports(self):
+        print("Sports skill from Son")
 
+obj = Son()
+obj.wisdom()
+obj.driving()
+obj.cooking()
+obj.sports()
 
-
-
-# class person:
-#     def __init__(self,name,age):
-#         self.name = name
-#         self.age = age
-#     def details(self):
-#         print(self.name,self.age)
-# class student(person):
-#     def __init__(self, name, age,gender):
-#         super().__init__(name, age)
-#         self.gender = gender
-#     def gen(self):
-#         print("name is :",self.name,"age is :",self.age,"gender is :",self.gender)
-# obj = student("raju",21,"male") 
-# obj.gen()
-
-
-        
